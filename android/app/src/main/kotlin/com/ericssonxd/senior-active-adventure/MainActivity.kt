c91a03ae4585f02a1ae89ab7af42f8497151c889
EricssonXD <114271127+EricssonXD@users.noreply.github.com>
2024-04-22 21:54:38 +0800

    worked

--- android/app/src/main/kotlin/com/ericssonxd/senior-active-adventure/MainActivity.kt
+++ android/app/src/main/kotlin/com/ericssonxd/senior-active-adventure/MainActivity.kt
@@ -1,11 +1,18 @@
+// package com.ericssonxd.senior_active_adventure
+
+// import androidx.appcompat.app.AppCompatActivity
+// import android.os.Bundle
+
+// class MainActivity : AppCompatActivity() {
+//     override fun onCreate(savedInstanceState: Bundle?) {
+//         super.onCreate(savedInstanceState)
+//         setContentView( R.layout.activity_main)
+//     }
+// }
+
 package com.ericssonxd.senior_active_adventure
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView( R.layout.activity_main)
-    }
+class MainActivity: FlutterActivity() {
 }
